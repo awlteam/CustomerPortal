98a37e91a21143ffff531e5052926a997e0f4731
Rupesh kumar <80203918+Rupesh93@users.noreply.github.com>
2022-03-09 00:03:23 +0530

    navbar

---
+++ src/components/home/home.js
@@ -0,0 +1,15 @@
+import Navbar from '../navbar/navbar'
+
+function Home(){
+    return(
+        <>
+         <div className="homecontainer">
+             <div className="homewrapper">
+                 
+             </div>
+         </div>
+
+        </>
+    )
+}
+export default Home;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,43 @@
+import './navbar.css'
+import {BiTimeFive } from "react-icons/bi";
+import {ImLocation2 } from "react-icons/im";
+import {IoIosCall } from "react-icons/io";
+
+import Logo from '../../image/logo.png'
+function Navbar(){
+    return(
+        <>
+          <div className="navcontainer">
+              <div className="navwrapper">
+                  <div className="navlogo">
+                      <img src={Logo} alt="" />
+                  </div>
+                  <div className="navabout">
+                      <div className="time">
+                          <div className="icon"> <BiTimeFive /></div>
+                          <div className="text">
+                               <div className="text1">Working Hours</div>
+                               <div className="text2">9 AM - 6:30 PM </div>
+                          </div>
+                      </div>
+                      <div className="navlocation">
+                          <div className="icon icon2"><ImLocation2 /></div>
+                          <div className="text">
+                               <div className="text1">Location</div>
+                               <div className="text2">Our Presence </div>
+                          </div>
+                      </div>
+                      <div className="navcall">
+                          <div className="icon icon2"><IoIosCall /></div>
+                          <div className="text">
+                               <div className="text1">Call Center</div>
+                               <div className="text2">9015-011-011</div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </>
+    )
+}
+export default Navbar;
